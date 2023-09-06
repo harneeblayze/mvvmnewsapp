a57c94f672bb95091e824f393b8aa3f2ecad42e1
harneeblayze <harneeblayze@gmail.com>
2023-09-06 11:23:23 +0100

    init setup

---
+++ app/src/main/java/com/codinginflow/mvvmnewsapp/features/breakingnews/BreakingNewsViewModel.kt
@@ -0,0 +1,82 @@
+package com.codinginflow.mvvmnewsapp.features.breakingnews
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.codinginflow.mvvmnewsapp.data.NewsArticle
+import com.codinginflow.mvvmnewsapp.data.NewsRepository
+import com.codinginflow.mvvmnewsapp.util.Resource
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.concurrent.TimeUnit
+import javax.inject.Inject
+
+@HiltViewModel
+class BreakingNewsViewModel @Inject constructor(
+    private val repository: NewsRepository
+) : ViewModel() {
+
+    private val eventChannel = Channel<Event>()
+    val events = eventChannel.receiveAsFlow()
+
+    private val refreshTriggerChannel = Channel<Refresh>()
+    private val refreshTrigger = refreshTriggerChannel.receiveAsFlow()
+
+    var pendingScrollToTopAfterRefresh = false
+
+    val breakingNews = refreshTrigger.flatMapLatest { refresh ->
+        repository.getBreakingNews(
+            refresh == Refresh.FORCE,
+            onFetchSuccess = {
+                pendingScrollToTopAfterRefresh = true
+            },
+            onFetchFailed = { t ->
+                viewModelScope.launch { eventChannel.send(Event.ShowErrorMessage(t)) }
+            }
+        )
+    }.stateIn(viewModelScope, SharingStarted.Lazily, null)
+
+    init {
+        viewModelScope.launch {
+            repository.deleteNonBookmarkedArticlesOlderThan(
+                System.currentTimeMillis() - TimeUnit.DAYS.toMillis(7)
+            )
+        }
+    }
+
+    fun onStart() {
+        if (breakingNews.value !is Resource.Loading) {
+            viewModelScope.launch {
+                refreshTriggerChannel.send(Refresh.NORMAL)
+            }
+        }
+    }
+
+    fun onManualRefresh() {
+        if (breakingNews.value !is Resource.Loading) {
+            viewModelScope.launch {
+                refreshTriggerChannel.send(Refresh.FORCE)
+            }
+        }
+    }
+
+    fun onBookmarkClick(article: NewsArticle) {
+        val currentlyBookmarked = article.isBookmarked
+        val updatedArticle = article.copy(isBookmarked = !currentlyBookmarked)
+        viewModelScope.launch {
+            repository.updateArticle(updatedArticle)
+        }
+    }
+
+    enum class Refresh {
+        FORCE, NORMAL
+    }
+
+    sealed class Event {
+        data class ShowErrorMessage(val error: Throwable) : Event()
+    }
+}
